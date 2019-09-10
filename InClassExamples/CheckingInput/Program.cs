9e077be34cfe3539fd06b2ebc252775b296d3519
Acke9387 <Adam@ou.edu>
2019-09-10 10:58:22 -0500

    Added an example for validating numeric input

---
+++ InClassExamples/CheckingInput/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckingInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your favorite # >>");
+            string answer = Console.ReadLine();
+            int answerAsInteger;
+
+            bool isSuccess = int.TryParse(answer, out answerAsInteger);
+            
+            if (isSuccess == true)
+            {
+                Console.WriteLine("Congratulations, you entered a valid number!");
+            }
+            else
+            {
+                Console.WriteLine("Sorry that was not a number, you lose");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
