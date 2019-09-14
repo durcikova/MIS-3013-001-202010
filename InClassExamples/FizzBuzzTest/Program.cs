f5fbb64d55ea1f8e327cc277f2628e1e6f8588ea
Alexandra Durcikova <alex@ou.edu>
2019-09-13 19:47:07 -0500

    Added FizzBuzz test.

---
+++ InClassExamples/FizzBuzzTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FizzBuzzTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+           //Console.WriteLine("This program will perform FizzBuzz on your lucky number. Let's see if you can figure out how FizzBuzz works...");
+           //Console.WriteLine("\nPlease input your lucky number now");
+
+        Console.WriteLine("Please input your lucky number");
+            string inputString = Console.ReadLine();
+            int LNB = Convert.ToInt32(inputString);
+          
+            if (LNB % 3 == 0 && LNB % 5 == 0)
+            {
+                Console.WriteLine(LNB + "Fizzbuzz");
+            }
+
+
+            else if (LNB % 3 == 0)
+            {
+                Console.WriteLine(LNB + "Fizz");
+            }
+            else if (LNB % 5 == 0)
+            {
+                Console.WriteLine(LNB + "Buzz");
+            }
+
+        
+    
+    
+}
---
+++ Participations/RandomGuess/Program.cs
@@ -0,0 +1,53 @@
+﻿/*Create a new console application (using the .NET Framework and not .NET Core).  
+ * Name your project and solution RandomGuess.  Generate a random number between 1 and 100, 
+ * 
+ * using the Random (Links to an external site.) class.  Prompt the user to guess the random number until 
+ * they successfully guess it.
+
+ Author = Alex
+
+Extra's:
+
+Allow the user to input the range for the random number (between X and Y)
+Validate that the user actually input a valid number, if they did not, prompt them to input a valid number
+ * 
+ * 
+ */
+
+using System;
+
+namespace RandomGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var rand = new Random();
+            int roll = rand.Next(1, 100);
+            Console.WriteLine(roll);
+
+
+
+            Console.WriteLine("Please guess a random number between 1 and 100");
+            string answerAsString = Console.ReadLine();
+            int Uanswer = Convert.ToInt32(answerAsString);
+
+
+
+            while (roll != Uanswer)
+
+            {
+
+                Console.WriteLine("Sorry, this is not the correct number");
+                Console.WriteLine("Please guess another random number between 1 and 100");
+                answerAsString = Console.ReadLine();
+                Uanswer = Convert.ToInt32(answerAsString);
+
+
+            }
+
+            Console.WriteLine("Congratulation! You guessed the correct number!");
+        }
+    }
+}
